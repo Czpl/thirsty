a9f86f241d261dfd2e21aa3670419c21054e4955
Mateusz <mateusz.czapel@gmail.com>
2023-08-13 20:43:04 +0200

    feat: initial firestore implementation

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -7,22 +7,21 @@ import { IPlant } from '../../interfaces/appInterfaces';
 import Tabs from '../Tabs/Tabs';
 import { NavLink, useNavigate } from 'react-router-dom'
 import useUserData from '../../hooks/useUserData';
+
 import { signOut } from "firebase/auth";
 import { auth } from '../../services/firebase';
+import { collection, addDoc, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
+import {db} from '../../services/firebase';
 
 function App() {
-  const [plants, setPlants] = useState(() => {
-    const savedPlants = localStorage.getItem("plants");
-    if(!savedPlants) return [];
-    return JSON.parse(savedPlants);
-  });
+  const [plants, setPlants] = useState<IPlant[]>([]);
   const [newPlantName, setNewPlantName] = useState('');
   const [newWateringInterval, setNewWateringInterval] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
   const navigate = useNavigate();
   const user = useUserData();
 
-  const handleNewPlant = () => {
+  const handleNewPlant = async () => {
     const id = uuidv1();
     const plantObj: IPlant = {
       id: id,
@@ -30,23 +29,50 @@ function App() {
       wateringInterval: Number(newWateringInterval)*24*60*60*1000,
       lastWateredTimestamp: Date.now()
     }
-    setPlants([...plants, plantObj])
+    try {
+      const docRef = await addDoc(collection(db, "plants"), {
+        ...plantObj,    
+      });
+      console.log("Document written with ID: ", docRef.id);
+      fetchPlants();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   }
   
+  const fetchPlants = async () => {
+    await getDocs(collection(db, "plants"))
+      .then((querySnapshot)=>{     
+        const newData = querySnapshot.docs
+          .map((doc) => ({...doc.data() as IPlant}));
+        setPlants([...newData]);     
+      })
+   
+  }
 
-  const plantUpdateCb = (plantid: string, action: string) => {
+  useEffect(()=>{
+    fetchPlants();
+  }, [])
+
+  const plantUpdateCb = async (plantid: string, action: string) => {
     if( action === PlantUpdateActions.remove ) {
-      setPlants(plants.filter((plant: IPlant) => plant.id !== plantid));
+      await getDocs(collection(db, "plants"))
+        .then((querySnapshot)=>{
+          const plantToRemove = querySnapshot.docs
+            .find((doc) => (doc.data().id == plantid));
+          plantToRemove && deleteDoc(plantToRemove.ref);
+        }).then(() => fetchPlants());
     }
     if (action === PlantUpdateActions.water ) {
-      const plantIdx = plants.findIndex((plant: IPlant) => plant.id === plantid);
-      const plantList = [...plants];
-      plantList[plantIdx].lastWateredTimestamp = Date.now();
-      setPlants(plantList);
+      await getDocs(collection(db, "plants"))
+        .then((querySnapshot)=>{
+          const plantToUpdate = querySnapshot.docs
+            .find((doc) => (doc.data().id == plantid))
+          plantToUpdate && updateDoc(plantToUpdate.ref, {lastWateredTimestamp: Date.now()});
+        }).then(() => fetchPlants());
     }
   }
 
- 
   const handleLogout = () => {               
     signOut(auth).then(() => {
       // Sign-out successful.
@@ -55,10 +81,6 @@ function App() {
       // An error happened.
     });
   }
-
-  useEffect(() => {
-    localStorage.setItem("plants", JSON.stringify(plants));
-  }, [plants]);
   
   useEffect(()=>{
     if (user) {
